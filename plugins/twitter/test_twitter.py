2f474442ac428b540eef88bbb5ba7c3d1053d84b
Celeste Ang <celesteanglm@gmail.com>
2025-01-21 09:41:46 +0800

    assign names to fns and split to 2 lines - shows reusability of fns

--- plugins/twitter/test_twitter.py
+++ plugins/twitter/test_twitter.py
@@ -19,25 +19,30 @@ twitter_plugin = TwitterPlugin(options)
 
 # Test case 1: Post a Tweet
 print("Running Test Case 1: Post a Tweet")
-twitter_plugin.get_function('post_tweet')("Hi! Hello world! This is a test tweet from the Twitter Plugin!")
+post_tweet_fn = twitter_plugin.get_function('post_tweet')
+post_tweet_fn("Hi! Hello world! This is a test tweet from the Twitter Plugin!")
 print("Posted tweet!")
 
 # Test case 2: Reply to a Tweet
 print("\nRunning Test Case 2: Reply to a Tweet")
-twitter_plugin.get_function('reply_tweet')(tweet_id=1879472470362816626, reply="Hey! This is a test reply!")
+reply_tweet_fn = twitter_plugin.get_function('reply_tweet')
+reply_tweet_fn(tweet_id=1879472470362816626, reply="Hey! This is a test reply!")
 print("Liked tweet!")
 
 # Test case 3: Like a Tweet
 print("\nRunning Test Case 3: Like a Tweet")
-twitter_plugin.get_function('like_tweet')(tweet_id=1879472470362816626)
+like_tweet_fn = twitter_plugin.get_function('like_tweet')
+like_tweet_fn(tweet_id=1879472470362816626)
 print("Liked tweet!")
 
 # Test case 4: Quote a Tweet
 print("\nRunning Test Case 4: Quote a Tweet")
-twitter_plugin.get_function('quote_tweet')(tweet_id=1879472470362816626, quote="Hey! This is a test quote tweet!")
+quote_tweet_fn = twitter_plugin.get_function('quote_tweet')
+quote_tweet_fn(tweet_id=1879472470362816626, quote="Hey! This is a test quote tweet!")
 print("Quoted tweet!")
 
 # Test case 5: Get Metrics
 print("\nRunning Test Case 5: Get Metrics")
-metrics = twitter_plugin.get_function('get_metrics')()
+get_metrics_fn = twitter_plugin.get_function('get_metrics')
+metrics = get_metrics_fn()
 print("Metrics:", metrics)
